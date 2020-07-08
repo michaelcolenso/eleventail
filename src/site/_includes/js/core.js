dc130bfb23751b69e797337ee67bb8110618bcfd
Michael Colenso <github@michaelcolenso.com>
2020-07-08 23:49:21 +0000

    updates

--- src/site/_includes/js/core.js
+++ src/site/_includes/js/core.js
@@ -1,10 +1,30 @@
-// simple button click event handler
-function btnHandler(selector, callback) {
-  var btn = document.querySelector(selector);
-  if(!btn) { return; }
-  btn.addEventListener('click', function(event) {
-    event.preventDefault();
-    callback();
-  }, false);
-}
+const form = document.querySelector('#form');
+form.addEventListener('submit', handleFormSubmission);
 
+async function handleFormSubmission(event) {
+  console.log(event)
+  event.preventDefault();
+  const form = new FormData(event.target);
+
+  const data = {
+    sku: form.get('sku'),
+    quantity: Number(form.get('quantity')),
+  };
+
+  const response = await fetch('/.netlify/functions/create-checkout', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  }).then((res) => res.json());
+
+  const stripe = Stripe(response.publishableKey);
+  const { error } = await stripe.redirectToCheckout({
+    sessionId: response.sessionId,
+  });
+
+  if (error) {
+    console.error(error);
+  }
+}
